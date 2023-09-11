b0ff0fc28746f794ebdf8424f50a7d51d5354071
Aoyon Mir <aoyon.mir@elev.ga.ntig.se>
2023-09-11 09:17:02 +0200

    La till divison med heltal och decimaltal

--- src/App.java
+++ src/App.java
@@ -1,5 +1,17 @@
+import java.util.Scanner;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        Scanner tangentbord = new Scanner (System.in);
+        
+        System.out.println("Divisions program:");
+        System.out.println("Ange tal 1:");
+        int tal1 = tangentbord.nextInt();
+        System.out.println("Ange tal2:");
+        int tal2 = tangentbord.nextInt();
+        double kvot = tal1/tal2;
+        double dkvot= (double)tal1/tal2;
+        System.out.println("Kvoten är: "+dkvot);
+        System.out.println("Kvoten är: "+kvot); 
     }
 }
